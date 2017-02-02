1b81d0f5d45b0275a2e20739d479a74390b35b56
Christian Lenke <christian.lenke@gamebook.io>
2017-02-02 14:13:25 +0100

    NEW: update and add some rjsx snippets

--- init/init-yasnippet.el
+++ init/init-yasnippet.el
@@ -4,6 +4,11 @@
 (use-package yasnippet
   :ensure t
   :diminish yas-minor-mode
+  :init
+  (defun capitalize-first (s)
+    (if (> (length s) 0)
+        (concat (upcase (substring s 0 1)) (substring s 1))
+      nil))
   :config
   (yas-global-mode 1)
   (setq yas-triggers-in-field t))
