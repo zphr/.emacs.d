a5c3f32ddab8c3b133c5e3a9dbbe7573aa183232
Christian Lenke <christian.lenke@gamebook.io>
2021-03-05 13:24:19 +0100

    Use the org package instead of the emacs included

--- init/init-org.el
+++ init/init-org.el
@@ -1,24 +1,19 @@
 ;;; ---------------------------------------- Org Mode
 
-(use-package org
+(use-package org-plus-contrib
   :ensure t
-  :defer t
-  :config
+  :bind ((:map org-mode-map
+	       ("M-C-l". org-metaright)
+	       ("M-C-h". org-metaleft)
+	       ("M-L". org-shiftright)
+	       ("M-H". org-shiftleft)))
+  :init
   (setq org-clock-persist 'history)
   (org-clock-persistence-insinuate)
 
   (setq org-directory "e:/Rettungsboot/Dokumente/TODO")
 
   (setq org-ellipsis " ▼")
-
-  (add-hook 'org-mode-hook
-	    (lambda ()
-	      (define-key org-mode-map (kbd "M-C-l") 'org-metaright)
-	      (define-key org-mode-map (kbd "M-C-h") 'org-metaleft)
-	      (define-key org-mode-map (kbd "M-L") 'org-shiftright)
-	      (define-key org-mode-map (kbd "M-H") 'org-shiftleft)
-	      )
-	    )
 
   (global-set-key (kbd "C-c l") 'org-store-link)
   (global-set-key (kbd "C-c C-l") 'org-insert-link)
@@ -31,7 +26,11 @@
   (setq org-use-speed-commands t)
   (setq org-completion-use-ido t)
   (setq org-src-fontify-natively t)
-  (setq org-hide-emphasis-markers t))
+  (setq org-hide-emphasis-markers t)
+
+  (require 'ol-git-link)
+
+  (require 'ox-confluence))
 
 
 ;;; ---------------------------------------- Org Bullet
